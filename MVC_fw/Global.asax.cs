40a1aa72583a30a26eb5c4d843035d5760b53f2d
Liu Link <ctliu5link@hotmail.com>
2024-07-08 11:47:45 +0800

    添加CustomViewEngine

--- MVC_fw/Global.asax.cs
+++ MVC_fw/Global.asax.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -16,6 +12,10 @@ namespace MVC_fw
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+            ViewEngines.Engines.Add(new Models.CustomViewEngine()); // Add the custom view engine
         }
     }
 }
---
+++ MVC_fw/Models/CustomViewEngine.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+namespace MVC_fw.Models
+{
+    public class CustomViewEngine : RazorViewEngine
+    {
+        public CustomViewEngine()
+        {
+            string[] locationFormats = new string[] {
+                "~/bin/Views/{1}/{0}.cshtml",
+                "~/bin/Views/Report/{0}.cshtml",
+            };
+#if Concatenate
+            // Concatenate the location formats
+            ConcatLocationFormats(locationFormats);
+#else
+            // Set the location formats for the view engine
+            MasterLocationFormats = PartialViewLocationFormats = ViewLocationFormats = locationFormats;
+#endif
+        }
+
+        public void ConcatLocationFormats(string[] locationFormats)
+        {
+            MasterLocationFormats = MasterLocationFormats.Concat(locationFormats).ToArray();
+            PartialViewLocationFormats = PartialViewLocationFormats.Concat(locationFormats).ToArray();
+            ViewLocationFormats = ViewLocationFormats.Concat(locationFormats).ToArray();
+        }
+    }
+}
